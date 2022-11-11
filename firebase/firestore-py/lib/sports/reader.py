62e38c6f33a1b2265e1838986e35e21da8786f04
Brayden K <brayk0hler@gmail.com>
2022-11-10 23:32:42 -0500

    took into account having 2023 data

--- firebase/firestore-py/lib/sports/reader.py
+++ firebase/firestore-py/lib/sports/reader.py
@@ -10,7 +10,7 @@ This handles reading sports.csv and its logic all in one (faster!)
 KEYWORDS = {
     "BVH": "Boys Varsity Hockey",
     "BJVH": "Boys JV Hockey",
-    
+
     "GVBB": "Girls Varsity Basketball",
     "GJVBB": "Girls JV Basketball",
 
@@ -86,7 +86,8 @@ def get_date(year, date):
     month = date[:date.index("/")]
     if len(month) == 1: # If the month has only 1 digit
         month = "0" + month # Make it 2 digits
-        
+    if int(month) <= 7:
+        year += 1
     day = date[date.index("/") +1:]
     if len(day) == 1:
         day = "0" + day
